051ab7d9681879e21d7cf848a44787d1347caf41
Amir Beshkar <amir.beshkar@gmail.com>
2020-07-07 21:32:27 +0430

    uncomment the actions

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,12 +1,12 @@
-// export * from "./features/auth/actions";
-// export { default as authActionTypes } from "./features/auth/actionTypes";
+export * from "./features/auth/actions";
+export { default as authActionTypes } from "./features/auth/actionTypes";
 
-// export * from "./features/socket/actions";
-// export { default as socketActionTypes } from "./features/socket/actionTypes";
+export * from "./features/socket/actions";
+export { default as socketActionTypes } from "./features/socket/actionTypes";
 
 export * from "./features/app/actions";
 export { default as appActionTypes } from "./features/app/actionTypes";
 
-// export * from "./features/profile/actions";
-// export * from "./features/profile/types";
-// export { default as profileActionTypes } from "./features/profile/actionTypes";
+export * from "./features/profile/actions";
+export * from "./features/profile/types";
+export { default as profileActionTypes } from "./features/profile/actionTypes";
