6012e2697a2ca3d5111674205ea1bb48e258b6af
Amir Beshkar <amir.beshkar@gmail.com>
2020-07-07 21:06:09 +0430

    reconfig typescript and update build command

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,11 +6,13 @@ const port = 4000;
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello world! "+req.path);
 });
 
-// start the Express server
+// // start the Express server
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
   console.log(redirectTo('https://sample.com'))
 });
+
+export default app
