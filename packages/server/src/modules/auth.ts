b46af242017192bc9243f78818eb22cf3d24604e
Amir Beshkar <amir.beshkar@gmail.com>
2020-07-12 16:45:56 +0430

    implement the login and verify token with jwt

--- packages/server/src/modules/auth.ts
+++ packages/server/src/modules/auth.ts
@@ -8,48 +8,55 @@ import {
   ProfileInfo,
   setProfileInfo,
   signOut,
-} from '@purple-messenger/core';
-import { dispatch } from 'core/helper/client';
-import type { Socket } from 'socket.io';
+} from "@purple-messenger/core";
+import { dispatch } from "core/helper/client";
+import Socket from "core/type/socket";
+import jwt from "core/helper/jwt";
 
-const demoAccount = 'demo';
-const testToken = 'test-token';
+const demoAccount = "demo";
+const testUserId = 1;
 const testProfileInfo: ProfileInfo = {
-  image: 'https://picsum.photos/200',
-  firstName: 'John',
-  lastName: 'Doe',
-  email: 'john@doe.demo',
+  image: "https://picsum.photos/200",
+  firstName: "John",
+  lastName: "Doe",
+  email: "john@doe.demo",
   bio: "Hi, My name is John Doe and I'm a demo user",
 };
 
 function auth(socket: Socket) {
+  socket.on(
+    authActionTypes.auth.saga.signIn,
+    ({ email, password }: SingInAction) => {
+      if (email === demoAccount && password === demoAccount) {
+        socket.session = { id: testUserId };
 
-  socket.on(authActionTypes.auth.saga.signIn, (message: SingInAction) => {
-    if (message.email === demoAccount && message.password === demoAccount) {
-      dispatch(socket, setToken(testToken));
-      dispatch(socket, setProfileInfo(testProfileInfo));
-    } else {
-      dispatch(socket, setAuthError('Email or Password is incorrect'));
-      dispatch(socket, setIsAuthenticated(false));
+        dispatch(socket, setToken(jwt.sign(socket.session)));
+        dispatch(socket, setProfileInfo(testProfileInfo));
+      } else {
+        dispatch(socket, signOut());
+        dispatch(socket, setAuthError("Email or Password is incorrect"));
+      }
     }
-  });
+  );
 
   socket.on(
     authActionTypes.auth.saga.verifyToken,
-    (message: VerifyTokenAction) => {
-      if (message.token === testToken) {
+    async ({ token }: VerifyTokenAction) => {
+      const session = await jwt.verify(token).catch(() => null);
+      if (!!session) {
+        socket.session = session;
         dispatch(socket, setIsAuthenticated(true));
         dispatch(socket, setProfileInfo(testProfileInfo));
       } else {
-        dispatch(socket, setAuthError('Invalid token'));
+        delete socket.session;
         dispatch(socket, signOut());
+        dispatch(socket, setAuthError("Invalid token"));
       }
-    },
+    }
   );
 
   socket.on(authActionTypes.auth.saga.signOut, () => {
-    // remove token from server
-    console.log('signout');
+    delete socket.session;
   });
 }
 
