42d6c13fe1a5668faa05eefdf736d8d57af1f7f3
Amir Beshkar <amir.beshkar@gmail.com>
2020-07-22 23:43:43 +0430

    improve the scrollbar logic and scroll to the end of messages if the last scroll position was at the end

--- packages/client/src/features/message/components/MessageList/MessageList.tsx
+++ packages/client/src/features/message/components/MessageList/MessageList.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useRef } from "react";
+import React, { memo, useEffect, useRef, useMemo, useLayoutEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getMessageList } from "features/message/selectors";
 import MessageRow from "../MessageRow";
@@ -13,13 +13,31 @@ function MessageList() {
   const messageListRef = useRef<HTMLDivElement>(null);
   const lastMessageRef = useRef<HTMLDivElement>(null);
   const classes = useStyles();
+  const isAtEndOfScroll = useRef(true);
 
   useEffect(() => {
     dispatch(clearMessageList());
   }, [username]);
 
-  useEffect(() => {
-    lastMessageRef.current?.scrollIntoView({behavior: 'smooth'});
+  isAtEndOfScroll.current = useMemo(() => {
+    const scrollbar = messageListRef.current;
+
+    if(!scrollbar) {
+      return false;
+    }
+
+    if(messages[messages.length-1].id<0) {
+      return true;
+    }
+
+    return scrollbar.offsetHeight + scrollbar.scrollTop >= scrollbar.scrollHeight
+  }, [messages]);
+
+
+  useLayoutEffect(() => {
+    if(isAtEndOfScroll.current) {
+      lastMessageRef.current?.scrollIntoView();
+    }
   }, [messages]);
 
   return (
