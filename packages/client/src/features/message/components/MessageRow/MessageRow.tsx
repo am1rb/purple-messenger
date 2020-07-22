654f05b5807eadfc2f7466a1647508212e083f26
Amir Beshkar <amir.beshkar@gmail.com>
2020-07-22 22:49:47 +0430

    add some details and styles to the message row component

--- packages/client/src/features/message/components/MessageRow/MessageRow.tsx
+++ packages/client/src/features/message/components/MessageRow/MessageRow.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { Box, Card } from "@material-ui/core";
-import { Message } from "@purple-messenger/core";
+import { Box, Card, Typography } from "@material-ui/core";
+import { Message, MessageOwner } from "@purple-messenger/core";
 import MessageStatus from "../MessageStatus";
+import MessageSentAt from "../MessageSentAt";
 
 export type Props = Message;
 
-function MessageRow({ body, status }: Props) {
+function MessageRow({ body, status, owner, sentAt }: Props) {
   return (
-    <Box m={1}>
+    <Box
+      m={2}
+      maxWidth="65%"
+      marginLeft={owner === MessageOwner.Me ? "auto" : undefined}
+      marginRight={owner === MessageOwner.Friend ? "auto" : undefined}
+    >
       <Card>
-        {body}
-        <Box display="flex">
-          <MessageStatus status={status} />
+        <Box m={1}>
+          <Typography gutterBottom variant="body2">{body}</Typography>
+          <Box display="flex" justifyContent="space-between">
+            {owner === MessageOwner.Me ? <MessageStatus status={status} /> : <span />}
+            <MessageSentAt sentAt={sentAt} />
+          </Box>
         </Box>
       </Card>
     </Box>
