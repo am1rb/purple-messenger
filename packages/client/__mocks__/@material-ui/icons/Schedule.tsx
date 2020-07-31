453aa7132bd49148b5034971d2744b7229202b3c
Amir Beshkar <amir.beshkar@gmail.com>
2020-07-31 23:25:57 +0430

    add a new unit-test item to the MessageStatus component

---
+++ packages/client/__mocks__/@material-ui/icons/Schedule.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const muiSchedule = jest.fn(() => <span data-testid="mui-icons-schedule" />);
+
+export default muiSchedule;
--- packages/client/src/features/message/components/MessageStatus/MessageStatus.test.tsx
+++ packages/client/src/features/message/components/MessageStatus/MessageStatus.test.tsx
@@ -6,6 +6,11 @@ import MessageStatus from "./MessageStatus";
 describe("The <MessageStatus /> tests", () => {
   it("Should display schedule icon if the state is pending", () => {
     const { getByTestId } = render(<MessageStatus status={Status.Pending} />);
+    expect(getByTestId("mui-icons-schedule")).toBeInTheDocument();
+  });
+
+  it("Should display schedule icon if the state is sent", () => {
+    const { getByTestId } = render(<MessageStatus status={Status.Sent} />);
     expect(getByTestId("mui-icons-done")).toBeInTheDocument();
   });
 });
