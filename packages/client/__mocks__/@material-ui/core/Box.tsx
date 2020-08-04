4896fbc561d684cd760bff8a7ebad9ed67800a60
Amir Beshkar <amir.beshkar@gmail.com>
2020-08-04 23:29:55 +0430

    mock the material-ui Box component

---
+++ packages/client/__mocks__/@material-ui/core/Box.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface BoxProps {
+  children: React.ReactNode;
+}
+
+const muiBox = jest.fn(({ children }: BoxProps) => <div data-testid="mui-box">{children}</div>);
+
+export default muiBox;
