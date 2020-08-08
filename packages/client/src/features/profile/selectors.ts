b662761b097aea6dd72534eddccc65dbf26398c6
Amir Beshkar <amir.beshkar@gmail.com>
2020-08-08 23:36:39 +0430

    add a new selector for profile data

--- packages/client/src/features/profile/selectors.ts
+++ packages/client/src/features/profile/selectors.ts
@@ -1,3 +1,6 @@
 import { State } from "./reducers";
 
 export const getProfileInfo = (state: State) => state.profile.profileInfo;
+
+export const getProfileInfoImage = (state: State) =>
+  getProfileInfo(state)?.image;
