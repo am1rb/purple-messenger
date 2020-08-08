4af993f32656a9f08173fbc40166a9997f2111e9
Amir Beshkar <amir.beshkar@gmail.com>
2020-08-08 23:37:24 +0430

    improve the renderWithStore function

--- packages/client/src/core/redux/configureStore.ts
+++ packages/client/src/core/redux/configureStore.ts
@@ -1,16 +1,16 @@
-import { createBrowserHistory } from 'history';
-import { applyMiddleware, createStore } from 'redux';
-import { routerMiddleware } from 'connected-react-router';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga';
+import { createBrowserHistory } from "history";
+import { applyMiddleware, createStore } from "redux";
+import { routerMiddleware } from "connected-react-router";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 
-import rootReducers, { State as RootState } from './reducers';
-import rootSagas from './sagas';
+import rootReducers, { State as RootState } from "./reducers";
+import rootSagas from "./sagas";
 
 export const history = createBrowserHistory();
 const sagaMiddleware = createSagaMiddleware();
 
-export default function configureStore(preloadedState?: RootState) {
+export default function configureStore(preloadedState?: Partial<RootState>) {
   const store = createStore(
     rootReducers(history),
     preloadedState,
--- packages/client/src/core/test.tsx
+++ packages/client/src/core/test.tsx
@@ -4,16 +4,22 @@ import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 import configureStore, { history } from "core/redux/configureStore";
 import { State as RootState } from "core/redux/reducers";
+import theme from "core/style/theme";
+import { ThemeProvider } from "@material-ui/styles";
 
 export function renderWithStore(
-  children: React.ReactNode,
+  ui: React.ReactElement,
   initStore: Partial<RootState>
 ) {
-  const store = { ...configureStore(), ...initStore };
+  const store = configureStore(initStore);
 
-  return render(
-    <Provider store={store}>
-      <ConnectedRouter history={history}>{children}</ConnectedRouter>
-    </Provider>
-  );
+  return render(ui, {
+    wrapper: ({ children }) => (
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>{children}</ConnectedRouter>
+        </Provider>
+      </ThemeProvider>
+    ),
+  });
 }
