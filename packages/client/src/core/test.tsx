8eaec2f98cb7f20178b45020a9188e602096987c
Amir Beshkar <amir.beshkar@gmail.com>
2020-08-11 23:49:21 +0430

    remove unnecessary wrapper from test renderer

--- packages/client/src/core/test.tsx
+++ packages/client/src/core/test.tsx
@@ -4,8 +4,6 @@ import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 import configureStore, { history } from "core/redux/configureStore";
 import { State as RootState } from "core/redux/reducers";
-import theme from "core/style/theme";
-import { ThemeProvider } from "@material-ui/styles";
 
 export function renderWithStore(
   ui: React.ReactElement,
@@ -15,11 +13,9 @@ export function renderWithStore(
 
   return render(ui, {
     wrapper: ({ children }) => (
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <ConnectedRouter history={history}>{children}</ConnectedRouter>
-        </Provider>
-      </ThemeProvider>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>{children}</ConnectedRouter>
+      </Provider>
     ),
   });
 }
