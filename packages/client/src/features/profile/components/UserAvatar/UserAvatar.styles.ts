0d6d51455bc6c5637392b00311309875001d77ac
Amir Beshkar <amir.beshkar@gmail.com>
2020-08-11 23:49:50 +0430

    update the UserAvatar tests

--- packages/client/src/features/profile/components/UserAvatar/UserAvatar.styles.ts
+++ packages/client/src/features/profile/components/UserAvatar/UserAvatar.styles.ts
@@ -1,10 +1,13 @@
 import { Theme } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 
-export default makeStyles((theme: Theme) => ({
-  root: {
-    width: theme.spacing(10),
-    height: theme.spacing(10),
-    margin: "0 auto",
-  },
-}));
+export default makeStyles(
+  (theme: Theme) => ({
+    root: {
+      width: theme.spacing(10),
+      height: theme.spacing(10),
+      margin: "0 auto",
+    },
+  }),
+  { name: "UserAvatar" }
+);
