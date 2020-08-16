79575b51102a01c4c538014f200c8d90c70b40d8
Amir Beshkar <amir.beshkar@gmail.com>
2020-08-16 21:19:27 +0430

    implement MessageIsTyping component and its mock

---
+++ packages/client/src/features/message/components/MessageIsTyping/MessageIsTyping.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MessageIsTyping() {
+  return <>Typing...</>;
+}
+
+export default MessageIsTyping;
---
+++ packages/client/src/features/message/components/MessageIsTyping/__mocks__/MessageIsTyping.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function messageIsTyping() {
+  return <span data-testid="message-is-typing" />;
+}
+
+export default messageIsTyping;
