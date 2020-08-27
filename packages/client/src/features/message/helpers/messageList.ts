96af445fa20bb801f430e2ecdf95812f2ed9bd9b
Amir Beshkar <amir.beshkar@gmail.com>
2020-08-28 00:40:43 +0430

    add some comments to the function

--- packages/client/src/features/message/helpers/messageList.ts
+++ packages/client/src/features/message/helpers/messageList.ts
@@ -6,9 +6,11 @@ export function mustScrollToTheEnd(
 ) {
   const messagesLen = messages.length;
 
+  // return true if a new message sent by me on this device
   if (messagesLen > 0 && messages[messagesLen - 1].id < 0) {
     return true;
   }
 
+  // return true if the scroll is at the end
   return scrollBar.offsetHeight + scrollBar.scrollTop >= scrollBar.scrollHeight;
 }
