d7ca80fa1856a65288c8c2b9f6dea0780002db50
Amir Beshkar <amir.beshkar@gmail.com>
2020-08-31 23:58:07 +0430

    write unit tests for the messageList functions

---
+++ packages/client/src/features/message/helpers/messageList.test.ts
@@ -0,0 +1,55 @@
+import {
+  mustScrollToTheEnd,
+  isMessageHasTemporaryId,
+  isScrollAtTheEnd,
+} from "./messageList";
+import { Message, MessageOwner, MessageStatus } from "@purple-messenger/core";
+
+const sharedMessage: Message = {
+  id: -1,
+  body: "1183 Veptat Grove",
+  owner: MessageOwner.Me,
+  sentAt: new Date(),
+  status: MessageStatus.Pending,
+};
+
+const sharedMessages: Message[] = [sharedMessage];
+
+const sharedScrollBar: Partial<HTMLDivElement> = {
+  offsetHeight: 600,
+  scrollTop: 400,
+  scrollHeight: 1000,
+};
+
+describe("The messageList tests", () => {
+  it("Should return true if the message has temporary id", () => {
+    expect(isMessageHasTemporaryId({ ...sharedMessage, id: -1 })).toBeTruthy();
+  });
+
+  it("Should return false if the message has not temporary id", () => {
+    expect(isMessageHasTemporaryId({ ...sharedMessage, id: 1 })).toBeFalsy();
+  });
+
+  it("Should render true if the scroll position is at the end", () => {
+    expect(isScrollAtTheEnd(sharedScrollBar as HTMLDivElement)).toBeTruthy();
+  });
+
+  it("Should render false if the scroll position is at the end", () => {
+    expect(
+      isScrollAtTheEnd({ ...sharedScrollBar, scrollTop: 0 } as HTMLDivElement)
+    ).toBeFalsy();
+  });
+
+  it("Should return true if the last message is sent by current user", () => {
+    const scrollBar: Partial<HTMLDivElement> = {};
+    expect(
+      mustScrollToTheEnd(scrollBar as HTMLDivElement, sharedMessages)
+    ).toBeTruthy();
+  });
+
+  it("Should return true if the scrollBar is at the end", () => {
+    expect(
+      mustScrollToTheEnd(sharedScrollBar as HTMLDivElement, sharedMessages)
+    ).toBeTruthy();
+  });
+});
--- packages/client/src/features/message/helpers/messageList.ts
+++ packages/client/src/features/message/helpers/messageList.ts
@@ -1,5 +1,13 @@
 import { Message } from "@purple-messenger/core";
 
+export function isMessageHasTemporaryId(message: Message) {
+  return message.id < 0;
+}
+
+export function isScrollAtTheEnd(scrollBar: HTMLDivElement) {
+  return scrollBar.offsetHeight + scrollBar.scrollTop >= scrollBar.scrollHeight;
+}
+
 export function mustScrollToTheEnd(
   scrollBar: HTMLDivElement,
   messages: Message[]
@@ -7,10 +15,10 @@ export function mustScrollToTheEnd(
   const messagesLen = messages.length;
 
   // return true if a new message sent by me on this device
-  if (messagesLen > 0 && messages[messagesLen - 1].id < 0) {
+  if (messagesLen > 0 && isMessageHasTemporaryId(messages[messagesLen - 1])) {
     return true;
   }
 
   // return true if the scroll is at the end
-  return scrollBar.offsetHeight + scrollBar.scrollTop >= scrollBar.scrollHeight;
+  return isScrollAtTheEnd(scrollBar);
 }
