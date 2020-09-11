2f4ff2ebc4501d14c90797ea60fd3a1148247352
Amir Beshkar <amir.beshkar@gmail.com>
2020-09-11 12:35:03 +0430

    fix an small issue in the IconButton mock file and usages

--- packages/client/src/__mocks__/@material-ui/core/IconButton.tsx
+++ packages/client/src/__mocks__/@material-ui/core/IconButton.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 
-export interface IconButtonProps {
+export interface IconButtonProps
+  extends Pick<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    "type" | "disabled"
+  > {
   onClick?: () => void;
   children: React.ReactNode;
 }
 
 function MuiIconButton(
-  { onClick, children }: IconButtonProps,
+  { type, disabled, onClick, children }: IconButtonProps,
   ref: React.Ref<HTMLButtonElement>
 ) {
   return (
-    <button data-testid="mock-mui-icon-button" onClick={onClick} ref={ref}>
+    <button
+      data-testid="mock-mui-icon-button"
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+      ref={ref}
+    >
       {children}
     </button>
   );
