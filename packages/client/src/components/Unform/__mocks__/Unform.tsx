e7b59e52e1889c9aab16d15604420aef65b55a65
Amir Beshkar <amir.beshkar@gmail.com>
2020-09-11 12:31:59 +0430

    simplify the Unform mock to collect the submit data form the real form fields

--- packages/client/src/components/Unform/__mocks__/Unform.tsx
+++ packages/client/src/components/Unform/__mocks__/Unform.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SubmitHandler } from "@rocketseat/unform";
+import { StringObject } from "core/type/general";
 
 interface UnformProps<T> {
   initialData?: Partial<T>;
@@ -7,11 +8,25 @@ interface UnformProps<T> {
   children: React.ReactNode;
 }
 
+const helpers = {
+  resetForm: () => {},
+};
+
 function Unform<T>({ initialData, onSubmit, children }: UnformProps<T>) {
-  return (
-    <form onSubmit={() => onSubmit(initialData as T, {} as any)}>
-      {children}
-    </form>
-  );
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data: StringObject = {};
+
+    const elements = event.currentTarget.elements;
+    for (let i = 0; i < elements.length; i++) {
+      const input = elements[i] as HTMLInputElement;
+      data[input.name] = input.value;
+    }
+
+    onSubmit(data as T, helpers as any);
+  }
+
+  return <form onSubmit={handleSubmit}>{children}</form>;
 }
 export default Unform;
