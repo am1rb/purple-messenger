1c898ad0c3e6cc96ddb678dac449179538a66ff8
Amir Beshkar <amir.beshkar@gmail.com>
2020-09-12 22:03:56 +0430

    change the UnformTextField mock to support label

--- packages/client/src/components/UnformTextField/__mocks__/UnformTextField.tsx
+++ packages/client/src/components/UnformTextField/__mocks__/UnformTextField.tsx
@@ -10,17 +10,30 @@ type TagProps = TextAreaProps & InputProps;
 
 export interface UnformTextFieldProps extends TagProps {
   fullWidth?: boolean;
+  label?: string;
 }
 
 function UnformTextField({
   multiline,
   fullWidth,
+  label,
   ...other
 }: UnformTextFieldProps) {
-  return multiline ? (
+  const field = multiline ? (
     <textarea data-testid="mock-unform-text-field" {...other} />
   ) : (
     <input data-testid="mock-unform-text-field" {...other} />
+  );
+
+  return label ? (
+    <>
+      <label>
+        {label}
+        {field}
+      </label>
+    </>
+  ) : (
+    field
   );
 }
 
