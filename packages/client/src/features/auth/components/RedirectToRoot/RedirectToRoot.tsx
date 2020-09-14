666a324e0569c6b07a9b091225c3eb0f6716cece
Amir Beshkar <amir.beshkar@gmail.com>
2020-09-14 22:35:25 +0430

    implement RedirectToRoot component

---
+++ packages/client/src/features/auth/components/RedirectToRoot/RedirectToRoot.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Redirect } from "react-router";
+
+function RedirectToRoot() {
+  return <Redirect to="/" />;
+}
+
+export default RedirectToRoot;
---
+++ packages/client/src/features/auth/components/RedirectToRoot/__mocks__/RedirectToRoot.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RedirectToRoot() {
+  return <span data-testid="mock-redirect-to-root" />;
+}
+
+export default RedirectToRoot;
