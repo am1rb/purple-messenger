f6a78b8dd4de2473524c69a33f6bf9653f694b74
Amir Beshkar <amir.beshkar@gmail.com>
2020-09-26 23:47:47 +0330

    reimplement the auth saga tests using redux-saga-test-plan API

--- packages/client/src/features/auth/sagas/auth.test.ts
+++ packages/client/src/features/auth/sagas/auth.test.ts
@@ -1,32 +1,21 @@
-import { fork, take, select, put } from "redux-saga/effects";
+import { select } from "redux-saga/effects";
+import { expectSaga } from "redux-saga-test-plan";
+import { verifyToken, connected, disconnected } from "@purple-messenger/core";
+import { getToken } from "features/auth/selectors";
 import { handleReconnect, rootSaga } from "./auth";
-import { socketActionTypes, verifyToken } from "@purple-messenger/core";
-import { getToken } from "../selectors";
 
 describe("The auth sagas tests", () => {
   it("Should fork the reconnect correctly", () => {
-    const rootGen = rootSaga();
-    expect(rootGen.next().value).toEqual(fork(handleReconnect));
+    expectSaga(rootSaga).fork(handleReconnect);
   });
 
   it("Should handle reconnect event and verify the token if exists", () => {
-    const gen = handleReconnect();
-
-    // wait for the disconnect event
-    expect(gen.next().value).toEqual(
-      take(socketActionTypes.socket.reducer.disconnected)
-    );
-
-    // wait for the connect event
-    expect(gen.next().value).toEqual(
-      take(socketActionTypes.socket.reducer.connected)
-    );
-
-    // get the token
-    expect(gen.next().value).toEqual(select(getToken));
-
-    // call the verifyToken action
     const token = "A token value";
-    expect(gen.next(token).value).toEqual(put(verifyToken(token)));
+    expectSaga(handleReconnect)
+      .provide([[select(getToken), token]])
+      .dispatch(disconnected())
+      .dispatch(connected())
+      .put(verifyToken(token))
+      .run();
   });
 });
