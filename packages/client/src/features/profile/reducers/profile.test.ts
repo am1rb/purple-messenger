e737485e1704d0af5e46c0c877819abfbd285e5e
Amir Beshkar <amir.beshkar@gmail.com>
2020-10-05 23:43:33 +0330

    write unit tests for the profile reducers

---
+++ packages/client/src/features/profile/reducers/profile.test.ts
@@ -0,0 +1,25 @@
+import {
+  setProfileInfo,
+  sampleProfileInfo,
+  resetProfileInfo,
+  signOut,
+} from "@purple-messenger/core";
+import reducer from "./profile";
+
+describe("The profile reducer tests", () => {
+  it("Should set profile info correctly", () => {
+    expect(reducer(undefined, setProfileInfo(sampleProfileInfo))).toEqual(
+      sampleProfileInfo
+    );
+  });
+
+  it("Should reset profile info correctly", () => {
+    const store = reducer(undefined, setProfileInfo(sampleProfileInfo));
+    expect(reducer(store, resetProfileInfo())).toBe(null);
+  });
+
+  it("Should reset profile info after signOut action", () => {
+    const store = reducer(undefined, setProfileInfo(sampleProfileInfo));
+    expect(reducer(store, signOut())).toBe(null);
+  });
+});
