e023a652eba75d7c609cb478f3b93dacd1cb1237
Amir Beshkar <amir.beshkar@gmail.com>
2020-10-15 22:45:09 +0330

    implement more methods and details for the socket.io-client mock

--- packages/client/src/__mocks__/socket.io-client/index.ts
+++ packages/client/src/__mocks__/socket.io-client/index.ts
@@ -8,25 +8,46 @@ class Socket {
 
   on = (event: string, func: Function) => {
     if (this.EVENTS[event]) {
-      return this.EVENTS[event].push(func);
+      this.EVENTS[event].push(func);
+    } else {
+      this.EVENTS[event] = [func];
     }
-    this.EVENTS[event] = [func];
+    return this;
+  };
+
+  off = (event: string, func?: Function) => {
+    if (this.EVENTS[event]) {
+      if (!func) {
+        delete this.EVENTS[event];
+      } else {
+        const idx = this.EVENTS[event].indexOf(func);
+        this.EVENTS[event].splice(idx, 1);
+      }
+    }
+    return this;
   };
 
   emit = (event: string, ...args: any[]) => {
     if (this.EVENTS[event]) {
       this.EVENTS[event].forEach((func) => func(...args));
     }
+    return this;
+  };
+
+  listeners = (event: string) => {
+    return this.EVENTS[event] ?? [];
   };
 
   connect = () => {
     this.connected = true;
     this.emit("connect");
+    return this;
   };
 
   disconnect = () => {
     this.connected = false;
     this.emit("disconnect");
+    return this;
   };
 }
 
