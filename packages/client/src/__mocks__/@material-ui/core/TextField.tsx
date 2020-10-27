c85f76d8081b934110b6715e9891258671a81f81
Amir Beshkar <amir.beshkar@gmail.com>
2020-10-27 22:44:57 +0330

    implement more detail in the TextField mock

--- packages/client/src/__mocks__/@material-ui/core/TextField.tsx
+++ packages/client/src/__mocks__/@material-ui/core/TextField.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import type { TextFieldProps } from "@material-ui/core/TextField";
 
-function MuiTextField() {
-  return <input type="text" data-testid="mock-mui-text-field" />;
+function MuiTextField({
+  helperText,
+  label,
+  defaultValue,
+  inputRef,
+}: TextFieldProps) {
+  const input = (
+    <>
+      <input
+        ref={inputRef}
+        type="text"
+        data-testid="mock-mui-text-field"
+        defaultValue={defaultValue as string}
+      />
+      {helperText && (
+        <span data-testid="mock-mui-text-field-helper-text">{helperText}</span>
+      )}
+    </>
+  );
+  return label ? (
+    <label>
+      <span data-testid="mock-mui-text-field-label">{label}</span>
+      {input}
+    </label>
+  ) : (
+    input
+  );
 }
 
 export default MuiTextField;
