fa7ca2f6b488b0974f03f2d408251263b4a6e136
Amir Beshkar <amir.beshkar@gmail.com>
2020-11-06 22:17:37 +0330

    complete the PageRouter logic

--- packages/client/src/components/PageRouter/PageRouter.test.tsx
+++ packages/client/src/components/PageRouter/PageRouter.test.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import PageRouter from "./PageRouter";
 
 describe("PageRouter Component Tests", () => {
   it("Should match the snapshot", () => {
-    render(<PageRouter />);
-    screen.debug();
+    const { container } = render(<PageRouter />);
+    expect(container).toMatchSnapshot();
   });
 });
