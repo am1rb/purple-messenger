ceac56c3b3cb401da093806722222ae0873b35c9
Amir Beshkar <amir.beshkar@gmail.com>
2020-11-12 23:47:48 +0330

    * remove an unnecessary test. * fix the reconnect test case.

--- packages/client/src/features/auth/sagas/auth.test.ts
+++ packages/client/src/features/auth/sagas/auth.test.ts
@@ -3,38 +3,30 @@ import { expectSaga, testSaga } from "redux-saga-test-plan";
 import {
   verifyToken,
   connected,
-  disconnected,
   socketActionTypes,
 } from "@purple-messenger/core";
 import { getToken } from "features/auth/selectors";
-import { handleReconnect, rootSaga } from "./auth";
+import { reconnect } from "./auth";
 
 describe("The auth sagas tests", () => {
-  it("Should fork the reconnect correctly", () => {
-    return expectSaga(rootSaga)
-      .fork(handleReconnect)
-      .run({ silenceTimeout: true });
-  });
-
   it("Should handle reconnect event and verify the token if exists", () => {
     const token = "A token value";
-    return expectSaga(handleReconnect)
+    return expectSaga(reconnect)
       .provide([[select(getToken), token]])
-      .dispatch(disconnected())
       .dispatch(connected())
       .put(verifyToken(token))
       .run({ silenceTimeout: true });
   });
 
   it("Should not call the verifyToken action if token is not available", () => {
-    testSaga(handleReconnect)
-      .next()
-      .take(socketActionTypes.socket.reducer.disconnected)
+    testSaga(reconnect)
       .next()
       .take(socketActionTypes.socket.reducer.connected)
       .next()
       .select(getToken)
       .next()
-      .take(socketActionTypes.socket.reducer.disconnected); // start of the loop
+      .isDone()
+      .next()
+      .finish();
   });
 });
