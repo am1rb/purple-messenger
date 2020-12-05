df777fdf97de92eedd722921b2244bf47cef991b
Amir Beshkar <amir.beshkar@gmail.com>
2020-12-05 23:28:41 +0330

    add message header to the message zone

--- packages/client/src/features/message/components/MessageZone/MessageZone.tsx
+++ packages/client/src/features/message/components/MessageZone/MessageZone.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Box } from "@material-ui/core";
 import SendMessageForm from "../SendMessageForm";
 import MessageList, { MessageListProps } from "../MessageList";
+import MessageHeader from "../MessageHeader";
 
 export interface MessageZoneProps extends MessageListProps {
   className?: string;
@@ -10,8 +11,17 @@ export interface MessageZoneProps extends MessageListProps {
 function MessageZone({ className, username }: MessageZoneProps) {
   return (
     <Box className={className} display="flex" flexDirection="column">
-      <Box flexGrow="1" flexShrink="1" overflow="hidden">
-        <MessageList username={username} />
+      <Box
+        display="flex"
+        flexDirection="column"
+        flexGrow="1"
+        flexShrink="1"
+        overflow="hidden"
+      >
+        <MessageHeader disabled={!username} />
+        <Box flexGrow="1" flexShrink="1" overflow="hidden">
+          <MessageList username={username} />
+        </Box>
       </Box>
       <Box flexGrow="0" flexShrink="0" m={1}>
         <SendMessageForm
